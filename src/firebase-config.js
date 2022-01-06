7db8f5ad61a16dac0d265e4db01ac30ef9d7fabb
Abhishek <abmishra700@gmail.com>
2022-01-06 17:01:51 +0530

    aaded react-router-dom v6

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,12 @@ const firebaseConfig = {
   projectId: "blog-14a51",
   storageBucket: "blog-14a51.appspot.com",
   messagingSenderId: "174596180764",
-  appId: "1:174596180764:web:14bd047e1e7133b64488d9"
+  appId: "1:174596180764:web:14bd047e1e7133b64488d9",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
