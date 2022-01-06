892e9c885d38f3f1e9a7efe0612b6256e7bb7316
Abhishek <abmishra700@gmail.com>
2022-01-06 16:48:41 +0530

    added css and firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8R-lDMUpAgZ5h3zV-v6CuyHAji1x5Y7Y",
+  authDomain: "blog-14a51.firebaseapp.com",
+  projectId: "blog-14a51",
+  storageBucket: "blog-14a51.appspot.com",
+  messagingSenderId: "174596180764",
+  appId: "1:174596180764:web:14bd047e1e7133b64488d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
