9b87b06d09ef0b09a66adeb2de3306bbc6d1d390
Abhishek <abmishra700@gmail.com>
2022-01-08 17:52:05 +0530

    get all posts from firebase and set in state

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  const [postList, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
 
-export default Home
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, [postsCollectionRef]);
+
+  return <div className="homePage"></div>;
+};
+
+export default Home;
