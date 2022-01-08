b6389dc3d9da4fa0eb74dd02dee28ac66f0d3015
Abhishek <abmishra700@gmail.com>
2022-01-08 17:18:07 +0530

    create post page

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const CreatePost = () => {
-    return (
-        <div>
-            CreatePost
-        </div>
-    )
-}
+  const [title, setTitle] = useState("");
+  const [post, setPost] = useState("");
 
-export default CreatePost
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(title, post);
+  };
+
+  return (
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create Post</h1>
+        <div className="inputGp">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            placeholder="Title..."
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="inputGp">
+          <label htmlFor="Post">Post</label>
+          <textarea
+            id="Post"
+            rows="10"
+            cols="50"
+            value={post}
+            placeholder="Post..."
+            onChange={(e) => setPost(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="createPostBtn" onClick={handleSubmit}>
+          Create Post
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
